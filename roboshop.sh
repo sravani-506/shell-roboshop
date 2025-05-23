0612acc385f74aaf472153269f72d5016d463d23
sravani-506 <bellagubbalasravani@gmail.com>
2025-05-23 23:16:11 +0530

    hello world started

---
+++ roboshop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0ba775c9adbd417a7" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z073037613RA3D1PBCCV8" # replace with your ZONE ID
+DOMAIN_NAME="likky.site" # replace with your domain
+
+for instance in ${INSTANCES[@]}
+#for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0ba775c9adbd417a7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+       
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+       
+    fi
+    echo "$instance IP address: $IP"
+done
